6164f6a198b678eccfff325ab032efb45da3cfa7
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2023-04-29 22:19:11 +0200

    Added footer

---
+++ ProgrammerHumor/src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <footer>
+        <p class="website-credit">Website built by <a href="https://github.com/WilliamNT" target="_blank">WilliamNT</a></p>
+    </footer>
+</template>
