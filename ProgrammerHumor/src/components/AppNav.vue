e3a40cfbcd37c2cb8d752ce91add78ccccf0a84e
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2023-04-29 20:47:50 +0200

    Removed demo components and added navbar

---
+++ ProgrammerHumor/src/components/AppNav.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+    import { library } from '@fortawesome/fontawesome-svg-core';
+    import { faDiscord, faReddit, faTwitch } from '@fortawesome/free-brands-svg-icons';
+    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+    library.add(faDiscord);
+    library.add(faReddit);
+    library.add(faTwitch);
+</script>
+
+<template>
+    <nav class="navbar">
+        <div class="navbar-brand">
+            <span>r/ProgrammerHumor</span>
+        </div>
+        <ul class="navbar-items">
+            <li class="navbar-item">
+                <a href="https://www.reddit.com/r/ProgrammerHumor/" target="_blank">
+                    <FontAwesomeIcon :icon="['fab', 'reddit']" class="icon" /> Subreddit
+                </a>
+            </li>
+            <li class="navbar-item">
+                <a href="https://discord.com/invite/rph" target="_blank">
+                    <FontAwesomeIcon :icon="['fab', 'discord']" class="icon" /> Discord server
+                </a>
+            </li>
+            <li class="navbar-item">
+                <a href="https://www.twitch.tv/programmerhumor" target="_blank">
+                  <FontAwesomeIcon :icon="['fab', 'twitch']" class="icon" /> Twitch channel
+                </a>
+            </li>
+        </ul>
+    </nav>
+</template>
---
+++ ProgrammerHumor/src/views/Home.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+    import AppNav from '../components/AppNav.vue';
+</script>
+
+<template>
+    <AppNav />
+</template>
