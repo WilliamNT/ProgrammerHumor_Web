8941025a6c35a594bf637c31c03ff0277dfd93a4
WilliamNT <71773977+WilliamNT@users.noreply.github.com>
2023-07-17 01:35:58 +0200

    Added EntropicBlackhole to the footer credit

--- ProgrammerHumor/src/components/AppFooter.vue
+++ ProgrammerHumor/src/components/AppFooter.vue
@@ -4,6 +4,6 @@
 
 <template>
     <footer>
-        <p class="website-credit">Website built by <a href="https://github.com/WilliamNT" target="_blank">WilliamNT</a></p>
+        <p class="website-credit">Website built by <a href="https://github.com/WilliamNT" target="_blank">WilliamNT</a> and <a href="https://github.com/EntropicBlackhole" target="_blank">EntropicBlackhole</a>.</p>
     </footer>
 </template>
