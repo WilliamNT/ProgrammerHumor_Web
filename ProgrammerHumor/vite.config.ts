1712f32f51284c43187a953ffe623b349e8cb05f
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-07-17 11:42:14 -0700

    remove: gh actions

--- ProgrammerHumor/vite.config.ts
+++ ProgrammerHumor/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ProgrammerHumor_Web/", // dont include in prod this is only for gh pages for elijah629 ( me )
+  base: "/",
   plugins: [vue()],
-})
+});
