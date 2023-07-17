f15891a71b6302aeb4b7267f6d164e9f3f502e53
elijah629 <62805599+elijah629@users.noreply.github.com>
2023-07-16 22:26:40 -0700

    Update vite.config.ts

--- ProgrammerHumor/vite.config.ts
+++ ProgrammerHumor/vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/ProgrammerHumor_Web/", // dont include in prod this is only for gh pages for elijah629 ( me )
   plugins: [vue()],
 })
